c6e21b6f51f967f7d1cd43fb09b22b291369d7ea
CarloBellettini <carlo.bellettini@unimi.it>
2014-05-06 19:07:07 +0200

    Portata in locale figura

--- assets/js/teplok-one-page.js
+++ assets/js/teplok-one-page.js
@@ -10,8 +10,8 @@ $(function(){
 $(function() {
 	$.vegas('slideshow', {
 	  backgrounds:[
-		{ src:'http://www.toptenz.net/wp-content/uploads/2013/10/raw-food-diet.jpg', fade:10000, delay: 400000 },
-		{ src:'http://www.zipheal.com/wp-content/uploads/2013/03/dukan_diet_foods1.jpg', fade:10000, delay: 400000 },
+		{ src:'http://www.toptenz.net/wp-content/uploads/2013/10/raw-food-diet.jpg', fade:3000, delay: 400000 },
+		{ src:'http://www.zipheal.com/wp-content/uploads/2013/03/dukan_diet_foods1.jpg', fade:3000, delay: 400000 },
 	  ]
 	})('overlay', {
 	  src:'assets/third/vegas/overlays/01.png'
